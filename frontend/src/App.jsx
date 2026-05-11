9deea68a6ba49ab06e39977597d71a2a73a7ea0c
Sarah Slayton <slayton4@illinois.edu>
2026-05-11 17:01:53 -0500

    fixed scrolling issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ function AppLayout() {
   const { pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scrollTo({ top: 0, behavior: 'instant' })
   }, [pathname])
 
   return (
